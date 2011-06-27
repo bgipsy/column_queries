73bceefd11133be932d7b6ab7acf4f6a3f0d57e4
Serge Balyuk <bgipsy@gmail.com>
2011-06-27 18:12:19 +0300

    more tests for db adapter

--- spec/lib/postgresql_adapter_extensions_spec.rb
+++ spec/lib/postgresql_adapter_extensions_spec.rb
@@ -4,13 +4,31 @@ describe ColumnQueries::PostgreSQLAdapterExtensions do
   
   include Generators
   
-  before :each do
-    generate_books 1..1000
+  before :all do
+    generate_books 1..20
   end
   
   it "should return array of ints" do
     result = Book.connection.select_int_values("SELECT id FROM books ORDER BY id")
-    result.should == (1..1000).to_a
+    result.should == (1..20).to_a
+  end
+  
+  it "should return array for each column" do
+    comment_ids, book_ids = Book.connection.select_columns_as_int_arrays("SELECT id, book_id FROM comments WHERE book_id=10")
+    book = Book.find(10)
+    
+    book_ids.should == [10] * book.comments.count
+    comment_ids.should =~ book.comment_ids
+  end
+  
+  it "should return 0 for NULL values" do
+    comment = Comment.first
+    Comment.create!(:body => 'another comment')
+    Comment.create!(:body => 'yet another comment')
+    
+    book_ids = Comment.connection.select_int_values("SELECT book_id FROM comments WHERE book_id IS NULL OR id=#{comment.id}")
+    
+    book_ids.should =~ [0, 0, comment.id]
   end
   
 end
--- spec/support/generators.rb
+++ spec/support/generators.rb
@@ -5,8 +5,16 @@ module Generators
       book = Book.new(:title => 'Lorem ipsum', :description => 'Lorem ipsum ' * 100, :price => 9.99)
       book.id = i
       book.save!
+      
+      generate_comments(book, 3)
     end
     Book.connection.reset_pk_sequence!('books')
+  end
+  
+  def generate_comments(book, ncomments)
+    ncomments.times do
+      Comment.create!(:book => book, :body => 'troll it!' * 10)
+    end
   end
   
 end
