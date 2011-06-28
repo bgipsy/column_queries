511ab74a377c3efd901cfdc3615913e281353523
Serge Balyuk <bgipsy@gmail.com>
2011-06-28 15:34:29 +0300

    specs for columns_as_int_arrays relation method

--- spec/lib/relation_extensions_spec.rb
+++ spec/lib/relation_extensions_spec.rb
@@ -11,21 +11,49 @@ describe ColumnQueries::RealtionExtensions do
     @books << Book.create!(:title => 'Book 4', :description => 'Lorem ipsum ' * 100, :price_cents =>  nil)
   end
   
-  it "should provide to_int_array method for scopes" do
-    Book.scoped.to_int_array(:id).should == @books.map(&:id)
+  describe "to_int_array" do
+    it "should work for scopes" do
+      Book.scoped.to_int_array(:id).should == @books.map(&:id)
+    end
+  
+    it "should work with names scopes" do
+      Book.pricy.to_int_array(:id).should == @books.select {|b| b.price_cents.to_i >= 1000}.map(&:id)
+    end
+  
+    it "should work with dynamic scopes" do
+      Book.scoped_by_price_cents(999).to_int_array(:id).should == [@books[1].id]
+      Book.where('price_cents = 999').to_int_array(:id).should == [@books[1].id]
+    end
+  
+    it "should work without arguments given .select() scope" do
+      Book.select(:price_cents).to_int_array.should == @books.map {|b| b.price_cents.to_i}
+    end
   end
   
-  it "should work with names scopes" do
-    Book.pricy.to_int_array(:id).should == @books.select {|b| b.price_cents.to_i >= 1000}.map(&:id)
-  end
+  describe "to_columns_as_int_arrays" do
+    it "should work for scopes" do
+      ids, price_cents = Book.scoped.to_columns_as_int_arrays(:id, :price_cents)
+      ids.should == @books.map(&:id)
+      price_cents.should == @books.map {|b| b.price_cents.to_i}
+    end
   
-  it "should work with dynamic scopes" do
-    Book.scoped_by_price_cents(999).to_int_array(:id).should == [@books[1].id]
-    Book.where('price_cents = 999').to_int_array(:id).should == [@books[1].id]
-  end
+    it "should work with names scopes" do
+      ids, price_cents = Book.pricy.to_columns_as_int_arrays(:id, :price_cents)
+      ids.should == @books.select {|b| b.price_cents.to_i >= 1000}.map(&:id)
+      price_cents.should == [1999]
+    end
   
-  it "should work without arguments given .select() scope" do
-    Book.select(:price_cents).to_int_array.should == @books.map {|b| b.price_cents.to_i}
+    it "should work with dynamic scopes" do
+      ids, price_cents = Book.scoped_by_price_cents(999).to_columns_as_int_arrays(:id, :price_cents)
+      ids.should == [@books[1].id]
+      price_cents.should == [999]
+    end
+  
+    it "should work without arguments given .select() scope" do
+      ids, price_cents = Book.select([:id, :price_cents]).to_columns_as_int_arrays
+      ids.should == @books.map(&:id)
+      price_cents.should == @books.map {|b| b.price_cents.to_i}
+    end
   end
   
 end
